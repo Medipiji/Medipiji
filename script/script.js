2d7f4f49543f4e3737425b46df6f71e8a4cb6283
Medipiji <jr_marcos17@outlook.com>
2025-03-31 02:07:58 -0300

    update layout

---
+++ script/script.js
@@ -0,0 +1,11 @@
+function menuInteract(){
+    let menu = document.querySelector(".mobile-menu");
+    if(menu){
+        console.log("foi")
+        menu.classList.toggle("showMenu");
+    }
+}
+
+function darkModeInteract(){
+    alert("ye")
+}
